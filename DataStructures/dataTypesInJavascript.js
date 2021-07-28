15ddeea1dab16baa9184e5530c723c54df963551
Prerna-31 <64802307+Prerna-31@users.noreply.github.com>
2021-07-28 18:09:00 +0530

    Add files via upload

---
+++ DataStructures/dataTypesInJavascript.js
@@ -0,0 +1,13 @@
+//Followings are the common data types we use in javascript:
+const num = 4 //The number or integer
+const dbl = 5.3 //The floating -point decimal
+const str = "String" // It is a string
+const bool = true  // Bollean value-> only two possible values--> true and false.
+const thing = null  //There is nothing in it.
+const something = undefined // There is something but it is underfined at this point of time.
+const list = ["item1", 34,5.6,[]] //Array which can contains list of items.
+const obj = {
+    'name' : "Bob",
+    'Age'  : 34
+}   // the object type having key-value pair. can be accessed as obj['name']
+const getAge = function() { return 34 ;} // function - typed
