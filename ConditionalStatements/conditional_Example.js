e01273c7f0e36052a0935fd3d54884cec5cae904
Prerna-31 <64802307+Prerna-31@users.noreply.github.com>
2021-07-28 18:07:43 +0530

    Add files via upload

---
+++ ConditionalStatements/conditional_Example.js
@@ -0,0 +1,31 @@
+// if - if
+let day = "Sunday"
+if (day == "Saturday")
+{
+   console.log("let's go to club having bollywood as theme");
+}
+if(day == "Sunday"){
+   console.log("let's go to club having hollywood as theme");
+}
+
+//if-else
+let cointoss = "Tail"
+if(cointoss == "Head"){
+    console.log("Let's study")
+}
+else{
+    console.log("Let's dance")
+}
+
+
+// if - else if - else
+let age = 19
+if(age >=18){
+    console.log("I AM READY TO VOTE")
+}
+else if(age >= 21 ){
+    console.log("I AM ALLOWED TO DRINK")
+}
+else{
+    console.log("NOT OLD ENOUGHT TO DRINK")
+}
