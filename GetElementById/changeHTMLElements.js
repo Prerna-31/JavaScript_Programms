61da9340785a5a3e34df3b31ea7e3b25e91b3bb0
Prerna-31 <64802307+Prerna-31@users.noreply.github.com>
2021-07-28 18:09:25 +0530

    Add files via upload

---
+++ GetElementById/changeHTMLElements.js
@@ -0,0 +1,4 @@
+let my_id = document.getElementById("para1")
+console.log(my_id)
+my_id.innerText = "This is the updated one"  // Html elements can't be used here as it considers it as text.
+// my_id.innerHTML = "I <strong> like </strong> coding"
